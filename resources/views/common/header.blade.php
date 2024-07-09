217d0eca52ee226b7725a2d58f8536432bb95987
Protatooo <henrich.boom@gmail.com>
2024-07-09 08:10:41 +0800

    aaa

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,52 +1,54 @@
-<div class="navbar bg-base-100">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16" />
-          </svg>
-        </div>
-        <ul
-          tabindex="0"
-          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li><a>Item 1</a></li>
-          <li>
-            <a>Parent</a>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </li>
-          <li><a>Item 3</a></li>
-        </ul>
-      </div>
-      <a class="btn btn-ghost text-xl">daisyUI</a>
+<div class="navbar bg-yellow-300">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">MaLaLim</a>
+  </div>
+  {{-- SEARCH BAR (MUST PLACE INTO 2ND NAVBAR) --}}
+  <div class="flex-none gap-2">
+    <div class="form-control">
+      <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
     </div>
-    <div class="navbar-center hidden lg:flex">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Item 1</a></li>
+
+    {{-- CART --}}
+    <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+      <div class="indicator">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+        </svg>
+
+        {{-- Place Cart Items here pag may time --}}
+        {{-- <span class="badge badge-sm indicator-item"></span> --}}
+      </div>
+    </div>
+    
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          {{-- Must Replace this with Registered Photo --}}
+          <img
+            alt="Profile Picture"
+            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        </div>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
         <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="p-2">
-              <li><a>Submenu 1</a></li>
-              <li><a>Submenu 2</a></li>
-            </ul>
-          </details>
+          <a class="justify-between">
+            Profile
+          </a>
         </li>
-        <li><a>Item 3</a></li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
       </ul>
     </div>
-    <div class="navbar-end">
-      <a class="btn">Button</a>
-    </div>
   </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myColor: {
+          50: "#fbfbef",
+          100: "#f7f8df",
+          200: "#eff0bf",
+          300: "#e7e99e",
+          400: "#dfe17e",
+          500: "#d7da5e",
+          600: "#acae4b",
+          700: "#818338",
+          800: "#565726",
+          900: "#2b2c13"
+        }
+      }
+
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: ["cupcake"],
+  },
+  plugins: [
+    require('flowbite/plugin'),
+    require('daisyui'),
+  ],
 }
 
