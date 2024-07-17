0526d5287267e23f52cc5e4d02861cbcffd424a3
Protatooo <henrich.boom@gmail.com>
2024-07-18 00:35:04 +0800

    logout

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -3,22 +3,142 @@
   <div class="flex-1">
     <a class="btn btn-ghost text-xl">MaLaLim</a>
   </div>
+<<<<<<< Updated upstream
     
     <button class="btn mr-3" onclick="my_modal_3.showModal()">
       <img src="../storage/cart.png" alt="Shopping Cart" style="width: 20px; height: 20px;" />
     </button>
     </button>
     <dialog id="my_modal_3" class="modal">
+=======
+  
+  {{-- SHOPPING CART --}}
+  <button class="btn" onclick="my_modal_3.showModal()">
+    <img src="{{ asset('storage/cart.png') }}" alt="Shopping Cart" style="width: 20px; height: 20px;" />
+  </button>
+
+  {{-- SHOPPING CARD MODAL --}}
+  <dialog id="my_modal_3" class="modal">
+>>>>>>> Stashed changes
       <div class="modal-box">
         <form method="dialog" class="">
           <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
         </form>
+<<<<<<< Updated upstream
         @include('common.cart')
       </div>
     </dialog>
 
     <button class="btn mr-3" onclick="window.location.href='{{ url('favorites') }}'">Favorites</button>            
     
+=======
+        <section class="h-screen py-12 sm:py-16 lg:py-20">
+  <div class="mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex items-center justify-center">
+      <h1 class="text-2xl font-semibold text-gray-900">Your Cart</h1>
+    </div>
+
+    <div class="mx-auto mt-8 max-w-md md:mt-12">
+      <div class="rounded-3xl bg-white shadow-lg">
+        <div class="px-4 py-6 sm:px-8 sm:py-10">
+          <div class="flow-root">
+            <ul class="-my-8">
+              <li class="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
+                <div class="shrink-0 relative">
+                  <span class="absolute top-1 left-1 flex h-6 w-6 items-center justify-center rounded-full border bg-white text-sm font-medium text-gray-500 shadow sm:-top-2 sm:-right-2">2</span>
+                  <img class="h-24 w-24 max-w-full rounded-lg object-cover" src="https://images.unsplash.com/photo-1588484628369-dd7a85bfdc38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNuZWFrZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=150&q=60" alt="Product Image 1" />
+                </div>
+
+                <div class="relative flex flex-1 flex-col justify-between">
+                  <div class="sm:col-gap-5 sm:grid sm:grid-cols-2">
+                    <div class="pr-8 sm:pr-5">
+                      <p class="text-base font-semibold text-gray-900">Nike Air Max 2019</p>
+                      <p class="mx-0 mt-1 mb-0 text-sm text-gray-400">36EU - 4US</p>
+                    </div>
+
+                    <div class="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                      <p class="shrink-0 w-20 text-base font-semibold text-gray-900 sm:order-2 sm:ml-8 sm:text-right">$1259.00</p>
+                    </div>
+                  </div>
+
+                  <div class="absolute top-0 right-0 flex sm:bottom-0 sm:top-auto">
+                    <button type="button" class="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900">
+                      <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </li>
+
+              <li class="flex flex-col space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
+                <div class="shrink-0 relative">
+                  <span class="absolute top-1 left-1 flex h-6 w-6 items-center justify-center rounded-full border bg-white text-sm font-medium text-gray-500 shadow sm:-top-2 sm:-right-2">1</span>
+                  <img class="h-24 w-24 max-w-full rounded-lg object-cover" src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=150&q=60" alt="Product Image 2" />
+                </div>
+
+                <div class="relative flex flex-1 flex-col justify-between">
+                  <div class="sm:col-gap-5 sm:grid sm:grid-cols-2">
+                    <div class="pr-8 sm:pr-5">
+                      <p class="text-base font-semibold text-gray-900">Nike Air Max 2019</p>
+                      <p class="mx-0 mt-1 mb-0 text-sm text-gray-400">36EU - 4US</p>
+                    </div>
+
+                    <div class="mt-4 flex items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                      <p class="shrink-0 w-20 text-base font-semibold text-gray-900 sm:order-2 sm:ml-8 sm:text-right">$1259.00</p>
+                    </div>
+                  </div>
+
+                  <div class="absolute top-0 right-0 flex sm:bottom-0 sm:top-auto">
+                    <button type="button" class="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900">
+                      <svg class="block h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                      </svg>
+                    </button>
+                  </div>`
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div class="mt-6 space-y-3 border-t border-b py-8">
+            <div class="flex items-center justify-between">
+              <p class="text-gray-400">Subtotal</p>
+              <p class="text-lg font-semibold text-gray-900">$2399.00</p>
+            </div>
+            <div class="flex items-center justify-between">
+              <p class="text-gray-400">Shipping</p>
+              <p class="text-lg font-semibold text-gray-900">$8.00</p>
+            </div>
+          </div>
+          <div class="mt-6 flex items-center justify-between">
+            <p class="text-sm font-medium text-gray-900">Total</p>
+            <p class="text-2xl font-semibold text-gray-900"><span class="text-xs font-normal text-gray-400">USD</span> 2499.00</p>
+          </div>
+
+          <div class="mt-6 text-center">
+            <button type="button" class="group inline-flex w-full items-center justify-center rounded-md bg-yellow-300 px-6 py-4 text-lg font-semibold ">
+              Place Order
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    </section>
+    </div>
+  </dialog>
+
+
+  {{-- LOGIN :: USER PROFILE --}}
+  @if (!Auth::user())
+    <button class="btn ml-3" onclick="window.location.href='{{ url('login') }}'">LOGIN</button>
+  @else
+>>>>>>> Stashed changes
+  {{-- LOGIN :: USER PROFILE --}}
+  @if (!Auth::user())
+    <button class="btn ml-3" onclick="window.location.href='{{ url('login') }}'">LOGIN</button>
+  @else  
     <div class="dropdown dropdown-end">
       <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
@@ -28,17 +148,21 @@
             src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
         </div>
       </div>
-      <ul
-        tabindex="0"
-        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+      <ul tabindex="0"class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
         <li>
           <a class="justify-between">
             Profile
           </a>
         </li>
         <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+
+        {{-- LOGOUT --}}
+        <li><a href="#" id="logoutLink">Logout</a></li>
+        
       </ul>
     </div>
-  </div>
+  @endif
+    
+
+
 </div>
