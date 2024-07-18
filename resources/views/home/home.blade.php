0736eeddcc339230c1708ef350a5ad952a8bcf58
Mark Bartolome <markalrayn.bartolome@tup.edu.ph>
2024-07-18 20:15:47 +0800

    lmao
    aaaaa

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -23,5 +23,15 @@
     {{-- <script src="{{ mix('js/app.js') }}"></script> --}}
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> <!-- Include jQuery -->
     
+    <button class="btn" onclick="my_modal_5.showModal()">View Product</button>
+<dialog id="my_modal_5" class="modal">
+  <div class="modal-box">
+    <form method="dialog">
+      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+    </form>
+    @include('common.productview')
+  </div>
+</dialog>
+
 </body>
 </html>
