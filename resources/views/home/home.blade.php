98e1866b8d9320ec706abb64a8c9c810e488fc03
Mark Bartolome <markalrayn.bartolome@tup.edu.ph>
2024-07-22 22:10:57 +0800

    banner2 type shit

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -15,7 +15,7 @@
    
     @include('common.header') <!-- Include header component -->
     @include('common.navbar') <!-- Include navbar component -->
-    @include('common.banner')
+    @include('common.banner2')
     <div id="jewelry" class="w-full mx-auto max-w-screen-xl p-4 md:grid md:grid-cols-4 gap-4 shadow-md">
         {{-- Content will be dynamically loaded here --}}
     </div>
